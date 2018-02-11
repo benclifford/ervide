a2b0b7152c66cd851cf5a39b544ec2b592558415
Ben Clifford <benc@hawaga.org.uk>
2018-02-11 15:55:13 +0000

    Use gen_server's timeout functionality instead of a periodic refresh

--- heater.erl
+++ heater.erl
@@ -1,6 +1,6 @@
 -module(heater).
 -behaviour(gen_server).
--export([init/1, handle_cast/2, handle_info/2]).
+-export([init/1, handle_cast/2, handle_info/2, terminate/2]).
 
 % How often the heater state will be retransmitted to the
 % heater control socket in the absence of any explicit
@@ -33,25 +33,22 @@ init([]) ->
   gpio:write(Pin16, 0),
   gpio:write(Pin13, 0),
 
-  erlang:send_after(?INTERVAL, self(), refresh),
-
   io:fwrite("heater: init finished\n"),
-  {ok, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, 0]}.
+  {ok, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, 0], ?INTERVAL}.
 
 handle_cast(Bit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit]) ->
   io:fwrite("heater: control power ~w on/off (was ~w)\n", [Bit, LastBit]),
   drive_radio(Bit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]),
 
   io:fwrite("heater: setting heater bit finished.\n"),
-  {noreply, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, Bit]}.
+  {noreply, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, Bit], ?INTERVAL}.
 
-handle_info(refresh, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit]) ->
-  io:fwrite("heater: handle_info: refreshing\n"),
+handle_info(timeout, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit]) ->
+  io:fwrite("heater: handle_info: refreshing remote power unit with current state\n"),
   drive_radio(LastBit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]),
   io:fwrite("heater: transmitter off\n", []),
 
-  erlang:send_after(?INTERVAL, self(), refresh),
-  {noreply, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit]}.
+  {noreply, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit], ?INTERVAL}.
 
 drive_radio(Bit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]) ->
   io:fwrite("heater: drive_radio: setting to ~w\n", [Bit]),
@@ -65,3 +62,10 @@ drive_radio(Bit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]) ->
   timer:sleep(250),
   gpio:write(Pin22, 0),
   io:fwrite("heater: drive_radio: transmitter off\n", []).
+
+
+terminate(Reason, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit]) ->
+  io:fwrite("heater: terminating, reason ~w\n", [Reason]),
+  drive_radio(LastBit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]),
+  io:fwrite("heater: termination complete\n", []).
+
