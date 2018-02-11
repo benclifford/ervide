439b8e5ce595781df61a48499a33466be318509d
Ben Clifford <benc@hawaga.org.uk>
2018-02-11 13:18:42 +0000

    Move heater control into own module

--- ervide.erl
+++ ervide.erl
@@ -1,7 +1,7 @@
 -module(ervide).
 -export([start/0,
 	 pwmmer/0, propctrl/0, integctrl/0, sumctrl/0,
-	 tempmeasure/0, errorctrl/0, heatctrl/0
+	 tempmeasure/0, errorctrl/0
 	]).
 
 startup_message() -> io:fwrite("ervide - an Erlang sous vide controller\n").
@@ -13,7 +13,7 @@ start() -> startup_message(),
 	   % not existing when it begins and sends its initial value.
 	   % That could be fixed...
 
-           Heater_process = spawn(ervide, heatctrl, []),
+           Heater_process = spawn(heater, heatctrl, []),
 	   io:fwrite("start: Heater_process = ~w (pid)\n", [Heater_process]),
 	   register(heater, Heater_process),
 
@@ -194,65 +194,3 @@ tempmeasure() ->
 
   timer:sleep(28014),
   tempmeasure().
-
-heatctrl() -> 
-  io:fwrite("heater: initialising GPIO pins\n"),
-
-  % PinNN numbers are board pins, but numbers
-  % passed to gpio:init are BCM numbers, I think.
-  Pin11 = gpio:init(17, out),
-  Pin13 = gpio:init(27, out),
-  Pin15 = gpio:init(22, out),
-  Pin16 = gpio:init(23, out),
-  Pin18 = gpio:init(24, out),
-  Pin22 = gpio:init(25, out),
-
-  gpio:write(Pin22, 0),
-
-% based on energenie's examples:
-
-%# Set the modulator to ASK for On Off Keying
-%# by setting MODSEL pin lo
-  gpio:write(Pin18, 0),
-
-%# Initialise K0-K3 inputs of the encoder to 0000
-  gpio:write(Pin11, 0),
-  gpio:write(Pin15, 0),
-  gpio:write(Pin16, 0),
-  gpio:write(Pin13, 0),
-
-  io:fwrite("heater: entering control loop\n"),
-  heaterloop(0, Pin11, Pin13, Pin15, Pin16, Pin18, Pin22).
-
-heaterloop(Last, Pin11, Pin13, Pin15, Pin16, Pin18, Pin22) ->
-  io:fwrite("heater: waiting for a message. last known state was ~w (on/off)\n", [Last]),
-
-  receive 
-    Bit -> io:fwrite("heater: control power ~w on/off\n", [Bit]),
-  gpio:write(Pin11, 0),
-  gpio:write(Pin15, 1),
-  gpio:write(Pin16, 1),
-  gpio:write(Pin13, Bit),
-  timer:sleep(250),
-  gpio:write(Pin22, 1),
-  io:fwrite("heater: transmitter on\n", []),
-  timer:sleep(250),
-  gpio:write(Pin22, 0),
-  io:fwrite("heater: transmitter off\n", []),
-           heaterloop(Bit, Pin11, Pin13, Pin15, Pin16, Pin18, Pin22)
-  after 60000 ->
-    io:fwrite("heater: control power refreshing present state of ~w (on/off)\n", [Last]),
-  gpio:write(Pin11, 0),
-  gpio:write(Pin15, 1),
-  gpio:write(Pin16, 1),
-  gpio:write(Pin13, Last),
-  timer:sleep(250),
-  gpio:write(Pin22, 1),
-  io:fwrite("heater: transmitter on\n", []),
-  timer:sleep(250),
-  gpio:write(Pin22, 0),
-  io:fwrite("heater: transmitter off\n", []),
-
-    heaterloop(Last, Pin11, Pin13, Pin15, Pin16, Pin18, Pin22)
-  end.
-
