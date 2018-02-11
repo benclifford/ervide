aa957596266fe6e59264df9582b5c26211e03432
Ben Clifford <benc@hawaga.org.uk>
2018-02-11 13:53:49 +0000

    heater refreshes state about every 5 seconds

--- heater.erl
+++ heater.erl
@@ -1,6 +1,11 @@
 -module(heater).
 -behaviour(gen_server).
--export([init/1, handle_cast/2]).
+-export([init/1, handle_cast/2, handle_info/2]).
+
+% How often the heater state will be retransmitted to the
+% heater control socket in the absence of any explicit
+% changes.
+-define(INTERVAL, 5634). % about 5 seconds
 
 init([]) -> 
   io:fwrite("heater: initialising GPIO pins\n"),
@@ -28,24 +33,35 @@ init([]) ->
   gpio:write(Pin16, 0),
   gpio:write(Pin13, 0),
 
+  erlang:send_after(?INTERVAL, self(), refresh),
+
   io:fwrite("heater: init finished\n"),
-  {ok, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]}.
+  {ok, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, 0]}.
 
-%  heaterloop(0, Pin11, Pin13, Pin15, Pin16, Pin18, Pin22).
+handle_cast(Bit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit]) ->
+  io:fwrite("heater: control power ~w on/off (was ~w)\n", [Bit, LastBit]),
+  drive_radio(Bit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]),
 
-handle_cast(Bit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]) ->
-  io:fwrite("heater: control power ~w on/off\n", [Bit]),
+  io:fwrite("heater: setting heater bit finished.\n"),
+  {noreply, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, Bit]}.
+
+handle_info(refresh, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit]) ->
+  io:fwrite("heater: handle_info: refreshing\n"),
+  drive_radio(LastBit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]),
+  io:fwrite("heater: transmitter off\n", []),
+
+  erlang:send_after(?INTERVAL, self(), refresh),
+  {noreply, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22, LastBit]}.
+
+drive_radio(Bit, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]) ->
+  io:fwrite("heater: drive_radio: setting to ~w\n", [Bit]),
   gpio:write(Pin11, 0),
   gpio:write(Pin15, 1),
   gpio:write(Pin16, 1),
   gpio:write(Pin13, Bit),
   timer:sleep(250),
   gpio:write(Pin22, 1),
-  io:fwrite("heater: transmitter on\n", []),
+  io:fwrite("heater: drive_radio: transmitter on\n", []),
   timer:sleep(250),
   gpio:write(Pin22, 0),
-  io:fwrite("heater: transmitter off\n", []),
-
-  io:fwrite("heater: setting heater bit finished.\n"),
-  {noreply, [Pin11, Pin13, Pin15, Pin16, Pin18, Pin22]}.
-
+  io:fwrite("heater: drive_radio: transmitter off\n", []).
